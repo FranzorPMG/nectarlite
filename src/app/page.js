e35b8afe912b8ee999571efd2ab15c48fb9cb630
MTA-DEV <stanleyatabiishabor@gmail.com>
2025-11-14 00:43:54 +0100

    MTA-DEV COMMIT

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,10 @@
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-import WorkingProcess from "./components/WorkingProcess";
-import Projects from "./components/Projects";
-import Pricing from "./components/Pricing";
-import Testimonials from "./components/Testimonials";
-import Blog from "./components/Blog";
-import SubFooter from "./components/SubFooter";
+import LenisScrollPage from "./components/LenisScrollPage";
+
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
-      <Hero />
-      <About />
-      <Services />
-      <Contact />
-      <WorkingProcess />
-      <Projects />
-      <Pricing />
-      <Testimonials />
-      <Blog />
-      <SubFooter />
+      <LenisScrollPage />
     </div>
   );
 }
